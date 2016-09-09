6247c6330c67b9e783cfeb8575209fb18a37569e
laojiaqi <lywhlao@qq.com>
2016-09-09 20:34:24 +0800

    实验csrf攻击

--- src/main/Java/web/control/HomeController.java
+++ src/main/Java/web/control/HomeController.java
@@ -1,24 +1,27 @@
 package web.control;
 
 import bean.UserEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import persistence.UserDao;
 
 @Controller
 public class HomeController {
 
+	private static final Logger THIRDPARTY_LOG = LoggerFactory.getLogger("THIRDPARTY_LOGGER");
 
 	@Autowired
 	UserDao userDao;
 
 	@RequestMapping(value = {"/","/home"},method = RequestMethod.GET)
-	public String home(@RequestParam("name")String name) {
-		userDao.findUserByName(name).getEmail();
+	public String home() {
 		return "home";
 	}
 
@@ -27,5 +30,19 @@ public class HomeController {
 		System.out.println("login"+name+","+password);
 		return "home";
 	}
+
+	@RequestMapping(value = {"/test"},method = RequestMethod.POST)
+	@ResponseBody
+	public String test() {
+		THIRDPARTY_LOG.info("test>>>>>>>>>>>>");
+		return "home";
+	}
+	@RequestMapping(value = {"/getTest"},method = RequestMethod.GET)
+	@ResponseBody
+	public String getTest() {
+		THIRDPARTY_LOG.info("test>>>>>>>>>>>>");
+		return "home";
+	}
+
 
 }
